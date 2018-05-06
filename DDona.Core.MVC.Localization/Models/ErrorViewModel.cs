51b257e3c8caf1f11f471cf3ba2f816c234e689f
diego.dona <diego.dona@outlook.com.br>
2018-05-05 21:40:18 -0300

    Add project files.

---
+++ DDona.Core.MVC.Localization/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DDona.Core.MVC.Localization.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
