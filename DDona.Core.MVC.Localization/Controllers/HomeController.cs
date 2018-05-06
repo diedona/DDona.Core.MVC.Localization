77cf8f81228f8e67c2c31678fb1ec785caa0eeb3
diego.dona <diego.dona@outlook.com.br>
2018-05-05 22:46:05 -0300

    opções de cultura

--- DDona.Core.MVC.Localization/Controllers/HomeController.cs
+++ DDona.Core.MVC.Localization/Controllers/HomeController.cs
@@ -5,11 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DDona.Core.MVC.Localization.Models;
+using Microsoft.Extensions.Localization;
 
 namespace DDona.Core.MVC.Localization.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IStringLocalizer _localizer;
+
+        public HomeController(IStringLocalizer<HomeController> localizer)
+        {
+            _localizer = localizer;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -17,7 +25,7 @@ namespace DDona.Core.MVC.Localization.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Descrição da página.";
+            ViewData["Message"] = _localizer["Page Description"];
 
             return View();
         }
--- DDona.Core.MVC.Localization/Startup.cs
+++ DDona.Core.MVC.Localization/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,17 +13,19 @@ namespace DDona.Core.MVC.Localization
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddLocalization(opts => opts.ResourcesPath = "Resources");
+            services.AddMvc()
+                .AddViewLocalization(opts => opts.ResourcesPath = "Resources");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +42,21 @@ namespace DDona.Core.MVC.Localization
             }
 
             app.UseStaticFiles();
+
+            IList<CultureInfo> cultures = new List<CultureInfo>()
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("pt-BR")
+            };
+
+            var requestLocalizationOptions = new RequestLocalizationOptions()
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                SupportedCultures = cultures,
+                SupportedUICultures = cultures
+            };
+
+            app.UseRequestLocalization(requestLocalizationOptions);
 
             app.UseMvc(routes =>
             {
